5d18bb0d85615d50c728622daefa0c93784a470b
Harish Saud <saudharish770@gmail.com>
2025-05-16 20:35:39 +0545

    Add files via upload

--- problem1.py
+++ problem1.py
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
+
 #Write a python program to display a user entered name followed by Good Afternoon using input () function.
 a=input("what is your name:")
-=======
+
 a=input("what is your name:")
->>>>>>> d59ea504d779ff119b73d44e77bec3371ca7b465
+
 print(f"Good afternoon {a}")
